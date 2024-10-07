e564ffd3bd1fa9b2545be0decbc8418ace5359da
James Angelo <jacsarona.official@gmail.com>
2024-10-07 19:17:55 +0800

    Implement increment and decrement with bloc

---
+++ lib/app_blocs.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/app_events.dart';
+import 'package:shop_app/app_states.dart';
+
+class AppBlocs extends Bloc<AppEvents, AppStates> {
+  AppBlocs() : super(InitStates()) {
+    on<Increment>((event, emit) {
+      emit(AppStates(counter: state.counter + 1));
+      print(state.counter);
+    });
+    on<Decrement>((event, emit) {
+      emit(AppStates(counter: state.counter - 1));
+    });
+  }
+}
