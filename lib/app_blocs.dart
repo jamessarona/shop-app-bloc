bd20a478cebb9b88505c97e853ecd7e11e6f6163
James Angelo <jacsarona.official@gmail.com>
2024-10-07 19:18:15 +0800

    Removed warnings

--- lib/app_blocs.dart
+++ lib/app_blocs.dart
@@ -6,7 +6,6 @@ class AppBlocs extends Bloc<AppEvents, AppStates> {
   AppBlocs() : super(InitStates()) {
     on<Increment>((event, emit) {
       emit(AppStates(counter: state.counter + 1));
-      print(state.counter);
     });
     on<Decrement>((event, emit) {
       emit(AppStates(counter: state.counter - 1));
