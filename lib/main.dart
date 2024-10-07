3fdc251eee274542ebe4a80ac1a07f3dd2288858
James Angelo <jacsarona.official@gmail.com>
2024-10-07 19:46:32 +0800

    Created welcome files

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shop_app/app_blocs.dart';
 import 'package:shop_app/app_events.dart';
 import 'package:shop_app/app_states.dart';
+import 'package:shop_app/pages/welcome/welcome.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,9 +17,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => AppBlocs(),
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: MyHomePage(),
+      child: ScreenUtilInit(
+        builder: (context, child) => const MaterialApp(
+            debugShowCheckedModeBanner: false, home: Welcome()),
       ),
     );
   }
---
+++ lib/pages/welcome/welcome.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Scaffold(
+        body: Container(
+          margin: EdgeInsets.only(top: 34.h),
+          width: 375.w,
+          child: Stack(
+            children: [
+              PageView(
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(
+                        width: 345.w,
+                        height: 345.w,
+                        child: const Text('Image one'),
+                      ),
+                      Container(
+                        child: Text(
+                          'First See Learning',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24.sp,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          'Forget about a for of paper all knowledge in one learning',
+                          style: TextStyle(
+                            color: Colors.black.withOpacity(0.5),
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
