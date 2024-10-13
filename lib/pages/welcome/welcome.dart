8f62049af60401324f36f8e7a11e16189baa7dd1
James Angelo <jacsarona.official@gmail.com>
2024-10-13 13:44:54 +0800

    Added animate to page in welcome page

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -14,6 +14,7 @@ class Welcome extends StatefulWidget {
 }
 
 class _WelcomeState extends State<Welcome> {
+  PageController pageController = PageController(initialPage: 0);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -27,6 +28,7 @@ class _WelcomeState extends State<Welcome> {
               alignment: Alignment.topCenter,
               children: [
                 PageView(
+                  controller: pageController,
                   onPageChanged: (index) {
                     state.page = index;
                     BlocProvider.of<WelcomeBloc>(context).add(WelcomeEvents());
@@ -83,68 +85,81 @@ class _WelcomeState extends State<Welcome> {
       )),
     );
   }
-}
 
-Widget _page(int index, BuildContext context, String buttonName, String title,
-    String subTitle, String imagePath) {
-  return Column(
-    children: [
-      SizedBox(
-        width: 345.w,
-        height: 345.w,
-        child: Image.asset(imagePath, fit: BoxFit.cover),
-      ),
-      Container(
-        child: Text(
-          title,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 24.sp,
-            fontWeight: FontWeight.normal,
-          ),
+  Widget _page(int index, BuildContext context, String buttonName, String title,
+      String subTitle, String imagePath) {
+    return Column(
+      children: [
+        SizedBox(
+          width: 345.w,
+          height: 345.w,
+          child: Image.asset(imagePath, fit: BoxFit.cover),
         ),
-      ),
-      Container(
-        width: 375,
-        padding: EdgeInsets.only(left: 30.w, right: 30.w),
-        child: Text(
-          subTitle,
-          style: TextStyle(
-            color: Colors.black.withOpacity(0.5),
-            fontSize: 14.sp,
-            fontWeight: FontWeight.normal,
-          ),
-        ),
-      ),
-      Container(
-        margin: EdgeInsets.only(top: 100, left: 25.w, right: 25.w),
-        width: 325.w,
-        height: 50.h,
-        decoration: BoxDecoration(
-          color: Colors.blue,
-          borderRadius: BorderRadius.all(
-            Radius.circular(15.w),
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.1),
-              spreadRadius: 10,
-              blurRadius: 20,
-              offset: const Offset(0, 5),
-            ),
-          ],
-        ),
-        child: Center(
+        Container(
           child: Text(
-            buttonName,
+            title,
             style: TextStyle(
-              color: Colors.white,
-              fontSize: 16.sp,
+              color: Colors.black,
+              fontSize: 24.sp,
               fontWeight: FontWeight.normal,
             ),
           ),
         ),
-      )
-    ],
-  );
+        Container(
+          width: 375,
+          padding: EdgeInsets.only(left: 30.w, right: 30.w),
+          child: Text(
+            subTitle,
+            style: TextStyle(
+              color: Colors.black.withOpacity(0.5),
+              fontSize: 14.sp,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            if (index < 3) {
+              pageController.animateToPage(
+                index,
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeIn,
+              );
+            } else {
+              // jump to a new page
+            }
+          },
+          child: Container(
+            margin: EdgeInsets.only(top: 100, left: 25.w, right: 25.w),
+            width: 325.w,
+            height: 50.h,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.all(
+                Radius.circular(15.w),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  spreadRadius: 10,
+                  blurRadius: 20,
+                  offset: const Offset(0, 5),
+                ),
+              ],
+            ),
+            child: Center(
+              child: Text(
+                buttonName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
 }
